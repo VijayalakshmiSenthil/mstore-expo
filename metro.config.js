b5cf1a6878167733080b538730521a555658e832
VijayalakshmiSenthil <vijayalakshmisenthil8133@gmail.com>
2023-12-21 20:35:28 +0530

    change commit

--- metro.config.js
+++ metro.config.js
@@ -9,9 +9,11 @@ module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
-        experimentalImportSupport: false,
+        experimentalImportSupport: true,
         inlineRequires: false,
       },
     }),
   },
 };
+
+
